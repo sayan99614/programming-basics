e4fd5e8b997e16cca858f592fd107a4407110afa
dheeman pati <dheemanpati53@gmail.com>
2022-01-04 12:59:12 +0530

    changed to final content

--- check.cpp
+++ check.cpp
@@ -2,10 +2,6 @@
 using namespace std;
 
 int main(){
-<<<<<<< HEAD
     cout<<"Hello world from master branch";
-=======
-    cout<<"Hello world from ubuntu";
->>>>>>> experimental
     return 0;
 }
